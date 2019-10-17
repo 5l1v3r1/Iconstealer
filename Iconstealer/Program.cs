7869b527bb198c66f015ad6a390411c7e353cc06
Jon Nowak <squirr3l@protonmail.com>
2019-10-17 19:41:18 +1300

    Add files

---
+++ Iconstealer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using Vestris.ResourceLib;
+
+namespace Iconstealer {
+    internal class Program {
+        internal static void StealIcon(string sourceExecutable, string targetExecutable) {
+            var sourceResInfo = new ResourceInfo();
+
+            File.Copy(targetExecutable, $"{targetExecutable}.bak");
+
+            sourceResInfo.Load(sourceExecutable);
+
+            foreach (var icon in sourceResInfo[Kernel32.ResourceTypes.RT_ICON]) {
+                icon.SaveTo(targetExecutable);
+            }
+
+            foreach (var groupico in sourceResInfo[Kernel32.ResourceTypes.RT_GROUP_ICON]) {
+                groupico.SaveTo(targetExecutable);
+            }
+        }
+
+        internal static void Main(string[] args) {
+            if (args.Length != 2) {
+                Console.WriteLine($"{typeof(Program).Assembly.Location} <source> <target>");
+                return;
+            }
+
+            StealIcon(args[0], args[1]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Iconstealer
+
+## Description
+
+Steals PE icon files from one executable and injects them into another!
  
+## Usage
+
+Iconstealer.exe <source> <target>
